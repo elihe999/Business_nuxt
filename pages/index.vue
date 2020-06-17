4316190b8b33efeabcaf6f574a8bbbd0877287ad
elihe999 <eli.he999@hotmail.com>
2020-06-17 17:40:21 +0800

    <chore>(break)Update pages index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,22 +12,15 @@
         <el-menu-item index="1">处理中心</el-menu-item>
         <el-submenu index="2">
           <template slot="title">我的工作台</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-3">选项3</el-menu-item>
-          <li v-for="good in goods" :key="good.id">
-            <nuxt-link :to="{name:'index-id',params:{id:good.id}}">
-              <span>{{good.text}}</span>
-              <span>￥{{good.price}}</span>
-              <button @click.prevent="addCart(good)">加购物车</button>
-            </nuxt-link>
-          </li>
-
+          <el-menu-item v-for="good in goods" index="good.index" :key="good.id">
+            <nuxt-link :to="{name: 'index-id', params: {id: good.id}}">{{good.text}}</nuxt-link>
+          </el-menu-item>
         </el-submenu>
         <el-menu-item index="3" disabled>消息中心</el-menu-item>
         <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
       </el-menu>
     </div>
+    <nuxt-child></nuxt-child>
   </div>
 </template>
 
@@ -38,14 +31,17 @@ export default {
   },
   data() {
     return {
+      activeIndex: '1',
       goods: [
-        {id:1, text: 'text1', price: 1111},
-        {id:2, text: 'text2', price: 2222}
+        {id: 1, text: 'text1', price: 1111, index: '2-1'},
+        {id: 2, text: 'text2', price: 2222, index: '2-2'}
       ]
     }
   },
   methods: {
-    addCart() {}
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
 }
 </script>
